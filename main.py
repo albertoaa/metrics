361491d160d169648703e6fe457f1a364c908858
Alberto Aragon Alvarez <ronin8503@gmail.com>
2017-09-06 22:58:43 -0500

    Rearranging elements inside main window

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from tkinter import *
 
 mainWindow = Tk()
+mainWindow.resizable(width=False, height=False)
 
-mainFrame = Frame(mainWindow)
+mainFrame = Frame(mainWindow, width=400, heigh=150)
+mainFrame.pack_propagate(0)
 mainFrame.pack()
 
-bottomFrame = Frame(mainWindow)
+topFrame = Frame(mainFrame)
+topFrame.pack(side=TOP)
+projectNameLabel = Label(topFrame, text="Proyecto Metrics")
+projectNameLabel.pack(side=LEFT)
+
+middleFrame = Frame(mainFrame)
+middleFrame.pack(fill=Y, expand=1)
+
+bottomFrame = Frame(mainFrame)
 bottomFrame.pack(side=BOTTOM)
 
-btnOscilospio = Button(mainFrame, text="OSCILOSCOPIO")
+btnOscilospio = Button(middleFrame, text="OSCILOSCOPIO")
 btnOscilospio.pack(side=LEFT)
-btnMultimetro = Button(mainFrame, text="MULTIMETRO")
+btnMultimetro = Button(middleFrame, text="MULTIMETRO")
 btnMultimetro.pack(side=LEFT)
-btnGenFunciones = Button(mainFrame, text="FUNCIONES")
+btnGenFunciones = Button(middleFrame, text="FUNCIONES")
 btnGenFunciones.pack(side=LEFT)
 
 
